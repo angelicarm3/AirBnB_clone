7f978e8c64324df4a2e2f5e0fe98604f13f4116a
Ella711 <allenro711@gmail.com>
2022-03-03 14:48:00 -0500

    feat: added comment and pycodestyle compliant

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,17 +28,23 @@ class FileStorage:
                     "State": State}
 
     def all(self):
-        """Returns the dictionary __objects"""
+        """
+        Returns the dictionary __objects
+        """
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        """
         if obj:
             key = '{}.{}'.format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
+        """
+        Serializes __objects to the JSON file (path: __file_path)
+        """
         dictionary = {}
         if self.__objects is not None:
             for key, value in self.__objects.items():
